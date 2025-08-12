34f225d102dae048d59f58e6902a39c51f50b360
quietjun802 <whdydwns802@gmail.com>
2025-08-12 15:15:33 +0900

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-import { useState } from 'react'
 import './App.css'
-import Counter from './components/Counter'
+import TodoList from './components/TodoList'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div>
-      <Counter/>
+      <TodoList/>
     </div>
   )
 }
